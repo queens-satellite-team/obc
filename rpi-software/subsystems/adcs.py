96be93bdc29372eff9eb98bd6dfdc45eda55445c
Hana Turcke <hanaturcke@leahurstcollege.ca>
2023-01-15 14:44:42 -0500

    create adcs class file

--- rpi-software/subsystems/adcs.py
+++ rpi-software/subsystems/adcs.py
@@ -1,24 +1,2 @@
-
 class ADCS:
-
-    # initialization method (like a constructor)
-    def __init__(self, param1, param2):
-        # examples of setting class attributes/fields
-        self.attribute1 = param1
-        self.attribute2 = param2
-        self.attribute3 = 2         # don't need to get all the values externally
-
-
-    # example method
-    def my_method(self, param1):    # needs a "self" parameter
-        print("hello")
-
-
-# creating an instance of an "ADCS" type object called "adcs"
-adcs = ADCS("something", "something else")
-
-# calling the "my_method" method on our ADCS instance
-adcs.my_method("param_value")
-
-# demonstrating accessing an attribute from a class instance
-print(adcs.attribute1)  
+    def __init__(self):
