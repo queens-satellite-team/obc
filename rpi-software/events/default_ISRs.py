39961c0f6289c1e57e0712893cde9acf80d29157
Hana Turcke <hanaturcke@leahurstcollege.ca>
2023-01-18 16:51:51 -0500

    OBC: structure and description for ground, fault, pass isr's

---
+++ rpi-software/events/default_ISRs.py
@@ -0,0 +1,41 @@
+"""
+Interrupt service routines for:
+    1. Ground-int
+    2. Fault-int
+    3. Pass sequences
+
+Call these once an interrupt has occurred (no interrupt checking here)
+
+"""
+
+def ground_isr():
+    # Receive MCC command
+
+    # transmit telemetry - send data from microSD and send to comms
+
+    # execute commands
+
+    # load new schedule
+
+    # send command to COMMS subsystem to clear the interrupt source
+
+
+    return
+
+def fault_isr():
+    # fault detection
+
+    # switch to fault-safe mode
+
+    return
+
+def pass_isr():
+    # roll to pointing angle
+
+    # check memory
+
+    # system health check
+
+    # proceed to pass mode
+
+    return
