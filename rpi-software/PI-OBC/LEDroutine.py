5dd10b64a9fd4f93f9d4ba7ee532476480b18667
jillibean <79066932+jillianmalherbe@users.noreply.github.com>
2023-05-28 19:23:44 -0400

    Add files via upload

---
+++ rpi-software/PI-OBC/LEDroutine.py
@@ -0,0 +1,91 @@
+from gpiozero import LED
+from time import sleep
+
+greenLED = LED(16)
+yellowLED = LED(20)
+redLED = LED(5)
+
+while True:
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    redLED.on()
+    sleep(0.2)
+    redLED.off()
+    sleep(0.2)
+    
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    redLED.on()
+    sleep(0.2)
+    redLED.off()
+    sleep(0.2)
+    
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    redLED.on()
+    sleep(0.2)
+    redLED.off()
+    sleep(0.2)
+
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    redLED.on()
+    sleep(0.2)
+    redLED.off()
+    sleep(0.2)
+    
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    yellowLED.on()
+    sleep(0.2)
+    yellowLED.off()
+    sleep(0.2)
+    
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    yellowLED.on()
+    sleep(0.2)
+    yellowLED.off()
+    sleep(0.2)
+    
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    yellowLED.on()
+    sleep(0.2)
+    yellowLED.off()
+    sleep(0.2)
+    
+    greenLED.on()
+    sleep(0.2)
+    greenLED.off()
+    sleep(0.2)
+    
+    yellowLED.on()
+    sleep(0.2)
+    yellowLED.off()
+    sleep(0.2)
+
+
+
+    
--- rpi-software/PI-OBC/obc_controller.py
+++ rpi-software/PI-OBC/obc_controller.py
@@ -1,7 +1,12 @@
 import yaml
 from Real_Time_Clock.rtc import RTC 
 from Temperature_Sensor.temperature_sensor import Temperature_Sensor
+#from Adafruit_ADC.Adafruit_MCP3008.MCP3008 import MCP3008
+from Adafruit_ADC.examples.mcp3008 import chan1 
+from Adafruit_ADC.examples.mcp3008 import chan2 
 import argparse
+import csv
+from csv import writer
 
 class OBC_Controller:
     """This is a class to contain various fucntions and operations of the on board controller. This class is meant to be used through a CLI.
@@ -23,7 +28,7 @@ class OBC_Controller:
     @staticmethod
     def init_rtc(config):
         rtc_config = config["rtc"]
-        return RTC(rtc_config['battery_state'],rtc_config['clock_state'],rtc_config['i2c_status'],rtc_config['datetime'])
+        return RTC(rtc_config['battery_state'],None,rtc_config['i2c_status'],rtc_config['datetime'])
 
     @staticmethod
     def init_temp(config):
@@ -31,18 +36,41 @@ class OBC_Controller:
         return Temperature_Sensor(temp_config["i2c_status"],temp_config["critical_temperature"],temp_config["upper_temperature"],temp_config["lower_temperature"])
 
     @staticmethod
+    def init_csv(config):
+        with open('telemetry_log.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Time", "Temperature", "Current", "Voltage"])
+
+
+#I don't know if this is needed for SPI device
+    @staticmethod
+    def init_ADC(config):
+        ADC_config = config["ADC"]
+        return Temperature_Sensor(temp_config["i2c_status"],temp_config["critical_temperature"],temp_config["upper_temperature"],temp_config["lower_temperature"])
+
+    @staticmethod
     def init_hardware():
         config = OBC_Controller.get_config()
         OBC_Controller.init_rtc(config)
         OBC_Controller.init_temp(config)
+        OBC_Controller.init_csv(config)
+    
 
     @staticmethod
     def get_telemetry():
         temp_interface = Temperature_Sensor()
         rtc_interface = RTC() 
+        current = chan1.voltage*10;
 
         print(f"Time: {rtc_interface.datetime}")
         print(f"OBC Ambient Temperature: {temp_interface.ambient} °C")
+        print(f"Current Monitor: {current} A")
+        print(f"Voltage Monitor: {chan2.voltage} V")
+        
+        with open('telemetry_log.csv', 'a') as f_object:
+            writer_object = writer(f_object)
+            writer_object.writerow([rtc_interface.datetime, temp_interface.ambient, current, chan2.voltage])
+            f_object.close()
 
 if __name__  == '__main__':
     FUNCTION_MAP =  {
